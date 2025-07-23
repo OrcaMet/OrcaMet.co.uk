97d1ea034c5a6fb200fc4008abd7e33141992626
UltraWorx24 <Michael@ultra-worx.com>
2025-07-23 11:47:47 +0000

    first run

---
+++ js/home.js
@@ -0,0 +1,9 @@
+// Shrink nav on scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('shrink');
+  } else {
+    navbar.classList.remove('shrink');
+  }
+});
