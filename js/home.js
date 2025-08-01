3a9a6934cfd52a82c58dc34a28eda2b535221bb8
UltraWorx24 <Michael@ultra-worx.com>
2025-08-01 21:06:58 +0000

    run test burger menu

--- js/home.js
+++ js/home.js
@@ -25,3 +25,12 @@ navLinks.forEach(link => {
     });
   }
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burger");
+  const nav = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    burger.classList.toggle("toggle");
+  });
+});
