05ba969db4977df5762b2fd761b7b7c6f764ec69
UltraWorx24 <Michael@ultra-worx.com>
2025-08-01 21:45:39 +0000

    push nav

--- js/services.js
+++ js/services.js
@@ -25,6 +25,9 @@ navLinks.forEach(link => {
     });
   }
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burger");
+  const nav = document.querySelector("nav");
 
   burger.addEventListener("click", () => {
     nav.classList.toggle("active");
